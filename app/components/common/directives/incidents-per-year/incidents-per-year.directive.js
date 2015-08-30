4b0039e06ca0a04563c63d944850b052f65d512c
Alex Klibisz <aklibisz@utk.edu>
2015-08-30 17:45:11 -0400

    graph implementation working, now need to start cleaning up

--- app/components/common/directives/incidents-per-year/incidents-per-year.directive.js
+++ app/components/common/directives/incidents-per-year/incidents-per-year.directive.js
@@ -9,32 +9,86 @@
       scope: {
         data: '=data',
         beginDate: '=beginDate',
-        endDate: '=endDate'
+        endDate: '=endDate',
+        countries: '=countries'
       },
       link: function(scope, element, attributes) {
 
-        function buildGraph(data) {
-          console.log(angular.toJson(data));
+        function formatData(data) {
+          var dataMap = {},
+            dataArray = [],
+            dataArraySorted = [];
 
-          var graph, years = [], counts = [], sortedCounts,
+          // Data is received in the format
+          // [{count:25, year:2012, country: "Malaysia"},
+          // {count: 35, year:2013, country: "Malaysia"}]
+
+          // Convert the data to the format
+          // {"Malaysia": { key: "Malaysia", values: [{year:2012, count: 25},{year:2013, count: 35}]}}
+          data.forEach(function(entry) {
+            if (!dataMap.hasOwnProperty(entry.country)) {
+              dataMap[entry.country] = {
+                key: entry.country,
+                values: []
+              };
+            }
+            dataMap[entry.country].values.push({
+              year: entry.year,
+              count: Number(entry.count)
+            });
+          });
+
+          // Convert the data to the format
+          // [{ key: "Malaysia", values: [{year:2012, count: 25},{year:2013, count: 35}]}]
+          Object.keys(dataMap).forEach(function(key) {
+            dataArray.push(dataMap[key]);
+          });
+
+          // Sort the data according to the aggregate count per country
+          dataArraySorted = dataArray.sort(function(countryA, countryB) {
+            var countryATotal = 0,
+              countryBTotal = 0;
+            countryA.values.forEach(function(value) {
+              countryATotal += value.count;
+            });
+            countryB.values.forEach(function(value) {
+              countryBTotal += value.count;
+            });
+            if (countryATotal < countryBTotal) return 1;
+            else if (countryATotal > countryBTotal) return -1;
+            else return 0;
+          });
+
+          return dataArray;
+        }
+
+        function buildGraph(data) {
+
+          var graph, years = [],
+            counts = [],
+            sortedCounts,
             minCount, maxCount;
 
           // Build an array of years to define the x-axis
-          for (var yr = scope.beginDate.getFullYear() - 1; yr <= scope.endDate.getFullYear() + 1; yr++) {
+          for (var yr = scope.beginDate.getFullYear(); yr <= scope.endDate.getFullYear(); yr++) {
             years.push(yr);
           }
 
           // Find the min and max counts to define the y-axis
-          data.forEach(function(country) {
-            country.values.forEach(function(value) {
-              counts.push(value.count);
+          minCount = 0;
+          maxCount = 0;
+          data.forEach(function(entry) {
+            entry.values.forEach(function(value) {
+              if(minCount === 0 || value.count < minCount) {
+                minCount = value.count;
+              }
+              if(maxCount === 0 || value.count > maxCount) {
+                console.log('new max', value.count);
+                maxCount = value.count;
+              }
             });
           });
-          sortedCounts = counts.sort();
-          minCount = sortedCounts[0];
-          maxCount = sortedCounts[sortedCounts.length - 1];
-
-          counts = [0, 50, 100, 150, 200];
+          console.log(minCount, maxCount);
 
           nv.addGraph(function() {
             graph = nv.models.lineChart().useInteractiveGuideline(true);
@@ -53,8 +107,12 @@
             graph.forceX([years[0], years[years.length - 1]]);
             graph.xAxis.tickValues(years);
 
-            graph.forceY([0, 200]);
-            graph.yAxis.tickValues([0, 50, 100, 150, 200]);
+            graph.forceY([minCount, maxCount]);
+            // graph.yAxis.tickValues([0, 50, 100, 150, 200]);
+
+            if(scope.countries.length === 0) {
+              data = data.splice(0,5);
+            }
 
             d3.select('.incidents-per-year-chart svg')
               .datum(data)
@@ -69,6 +127,7 @@
         scope.$watch(function() {
           return scope.data;
         }, function(data) {
+          data = formatData(data);
           buildGraph(data);
         });
       }
