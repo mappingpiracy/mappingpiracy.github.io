96889d2800eae7912a7cb7ba8b304a2f4b5d2cdf
Alex Klibisz <aklibisz@utk.edu>
2015-08-30 17:57:17 -0400

    refactored incidents per year directive

--- app/components/common/directives/incidents-per-year/incidents-per-year.directive.js
+++ app/components/common/directives/incidents-per-year/incidents-per-year.directive.js
@@ -1,5 +1,11 @@
+/**
+ * incidents-per-year directive
+ *
+ */
 (function() {
   'use strict';
+
+  mp.directive('incidentsPerYear', incidentsPerYear);
 
   function incidentsPerYear() {
     return {
@@ -14,18 +20,26 @@
       },
       link: function(scope, element, attributes) {
 
+        // Rebuild the chart anytime the data changes
+        scope.$watch(function watchData() {
+          return scope.data;
+        }, function newData(data) {
+          var formattedData = formatData(data);
+          buildGraph(formattedData);
+        });
+
         function formatData(data) {
           var dataMap = {},
             dataArray = [],
             dataArraySorted = [];
 
-          // Data is received in the format
+          // Data is received in the format:
           // [{count:25, year:2012, country: "Malaysia"},
-          // {count: 35, year:2013, country: "Malaysia"}]
+          // {count: 35, year:2013, country: "Malaysia"}, ...]
 
-          // Convert the data to the format
-          // {"Malaysia": { key: "Malaysia", values: [{year:2012, count: 25},{year:2013, count: 35}]}}
-          data.forEach(function(entry) {
+          // First pass: convert the data to the format:
+          // {"Malaysia": { key: "Malaysia", values: [{year:2012, count: 25},{year:2013, count: 35}, ...]}, ...}
+          data.forEach(function formatDataFirstPass(entry) {
             if (!dataMap.hasOwnProperty(entry.country)) {
               dataMap[entry.country] = {
                 key: entry.country,
@@ -38,16 +52,15 @@
             });
           });
 
-          // Convert the data to the format
+          // Second pass: convert the data to the format:
           // [{ key: "Malaysia", values: [{year:2012, count: 25},{year:2013, count: 35}]}]
-          Object.keys(dataMap).forEach(function(key) {
+          Object.keys(dataMap).forEach(function formatDataSecondPass(key) {
             dataArray.push(dataMap[key]);
           });
 
-          // Sort the data according to the aggregate count per country
-          dataArraySorted = dataArray.sort(function(countryA, countryB) {
-            var countryATotal = 0,
-              countryBTotal = 0;
+          // Sort the data according to the total count per country
+          dataArraySorted = dataArray.sort(function dataArraySorting(countryA, countryB) {
+            var countryATotal = 0, countryBTotal = 0;
             countryA.values.forEach(function(value) {
               countryATotal += value.count;
             });
@@ -59,79 +72,62 @@
             else return 0;
           });
 
-          return dataArray;
+          return dataArraySorted;
         }
 
         function buildGraph(data) {
 
-          var graph, years = [],
-            counts = [],
-            sortedCounts,
-            minCount, maxCount;
+          var graph, years = [], sortedCounts, maxCount = 0;
 
           // Build an array of years to define the x-axis
           for (var yr = scope.beginDate.getFullYear(); yr <= scope.endDate.getFullYear(); yr++) {
             years.push(yr);
           }
 
-          // Find the min and max counts to define the y-axis
-          minCount = 0;
-          maxCount = 0;
-          data.forEach(function(entry) {
+          // Find the max count to define the y-axis
+          data.forEach(function findMaxCount(entry) {
             entry.values.forEach(function(value) {
-              if(minCount === 0 || value.count < minCount) {
-                minCount = value.count;
-              }
               if(maxCount === 0 || value.count > maxCount) {
-                console.log('new max', value.count);
                 maxCount = value.count;
               }
             });
           });
-          console.log(minCount, maxCount);
 
-          nv.addGraph(function() {
+          // Build/rebuild the graph using the nv-d3 library
+          nv.addGraph(function addNVD3Graph() {
             graph = nv.models.lineChart().useInteractiveGuideline(true);
 
+            // Define the properties for x and y axes to use as values
             graph.x(function(d) {
               return d.year;
             });
-
             graph.y(function(d) {
               return d.count;
             });
 
+            // Name and define ticks for x and y axes
+            // x-axis uses years, y-axis uses count
             graph.xAxis.axisLabel('Year');
-            graph.yAxis.axisLabel('Incident Count');
-
             graph.forceX([years[0], years[years.length - 1]]);
             graph.xAxis.tickValues(years);
 
-            graph.forceY([minCount, maxCount]);
-            // graph.yAxis.tickValues([0, 50, 100, 150, 200]);
+            graph.yAxis.axisLabel('Incident Count');
+            graph.forceY([0, maxCount]);
 
+            // If no countries were specified, only the first five should be shown
             if(scope.countries.length === 0) {
               data = data.splice(0,5);
             }
 
-            d3.select('.incidents-per-year-chart svg')
+            // Select the element to which the data will be added and add it
+            d3.select('.incidents-per-year-graph > svg')
               .datum(data)
               .call(graph);
-
-            nv.utils.windowResize(graph.update);
 
             return graph;
           });
         }
-        // Rebuild the chart anytime the data changes
-        scope.$watch(function() {
-          return scope.data;
-        }, function(data) {
-          data = formatData(data);
-          buildGraph(data);
-        });
       }
     };
   }
-  mp.directive('incidentsPerYear', incidentsPerYear);
 })();
