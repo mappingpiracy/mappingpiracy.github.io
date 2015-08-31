7311ecf1311b99fdc7b723d8caf14a20446e1031
Alex Klibisz <aklibisz@utk.edu>
2015-08-31 02:43:30 -0400

    removing bower-downloaded packages, replacing with cdn scripts

--- gulpfile.js
+++ gulpfile.js
@@ -19,17 +19,37 @@ var paths = {
     src: ['./index.html', './app/components/*.html'],
     templates: 'app.templates.js'
   },
-  js: {
+  scripts: {
     src: ['./app/app*.js', './app/components/**/*.js'],
     min: 'app.min.js'
   },
-  sass: {
+  styles: {
     src: ['./app/components/**/*.scss'],
     min: 'app.min.css'
+  },
+  lib: {
+    scripts: {
+      src: [
+        'app/lib/angular/angular.js',
+        'app/lib/angular-route/angular-route.js',
+        'app/lib/jquery/dist/jquery.js',
+        'app/lib/d3/d3.js',
+        'app/lib/nvd3/nv.d3.min.js',
+        'app/lib/leaflet/dist/leaflet.js',
+        'app/lib/file-saver/FileSaver.min.js',
+        'app/lib/sheetrock/dist/sheetrock.min.js',
+        'app/lib/angular-ui-bootstrap/ui-bootstrap-custom-0.13.3.min.js',
+        'app/lib/angular-ui-bootstrap/ui-bootstrap-custom-tpls-0.13.3.min.js',
+        'app/lib/moment/min/moment.min.js',
+        'app/lib/modernizr/modernizr.js',
+        'app/lib/webshim/scripts-webshim/minified/polyfiller.js'
+      ],
+      min: 'lib.min.js'
+    }
   }
 };
 
-gulp.task('build', ['js-build', 'js-inject', 'sass-build', 'strip-debug']);
+gulp.task('build', ['scripts-build', 'styles-build', 'lib-build', 'strip-debug', 'inject']);
 
 gulp.task('html-build', function() {
   gulp.src('./app/components/**/*.html')
@@ -41,45 +61,56 @@ gulp.task('html-build', function() {
     .pipe(gulp.dest(paths.app));
 });
 
-gulp.task('js-inject', function() {
+gulp.task('inject', function() {
   var target = gulp.src(paths.index),
-    sources = gulp.src(paths.build + '/' + paths.js.min, {
+    sources = gulp.src([
+      // paths.build + '/' + paths.lib.scripts.min,
+      paths.build + '/' + paths.scripts.min,
+      paths.build + '/' + paths.styles.min
+    ], {
       read: false
     });
   return target.pipe(inject(sources))
     .pipe(gulp.dest(paths.base));
 });
 
-gulp.task('js-build', function() {
-  return gulp.src(paths.js.src)
-    .pipe(gconcat(paths.js.min))
+gulp.task('lib-build', function() {
+  gulp.src(paths.lib.scripts.src)
+    .pipe(gconcat(paths.lib.scripts.min))
     .pipe(uglify())
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('js-sourcemaps', function() {
-  return gulp.src(paths.js.min)
+gulp.task('scripts-build', function() {
+  return gulp.src(paths.scripts.src)
+    .pipe(gconcat(paths.scripts.min))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('scripts-sourcemaps', function() {
+  return gulp.src(paths.scripts.min)
     .pipe(gsourcemaps.init())
     .pipe(gsourcemaps.write())
     .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('strip-debug', function() {
-  return gulp.src([paths.js.min, paths.sass.min])
+  return gulp.src([paths.scripts.min, paths.styles.min])
     .pipe(gstripdebug())
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('sass-build', function() {
-  gulp.src(paths.sass.src)
+gulp.task('styles-build', function() {
+  gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(gcssmin())
-    .pipe(grename(paths.sass.min))
+    .pipe(grename(paths.styles.min))
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('sass-sourcemaps', function() {
-  gulp.src(paths.sass.min)
+gulp.task('styles-sourcemaps', function() {
+  gulp.src(paths.styles.min)
     .pipe(gsourcemaps.init())
     .pipe(gsourcemaps.write())
     .pipe(gulp.dest(paths.build));
@@ -92,8 +123,8 @@ gulp.task('watch', function() {
   gulp.watch(paths.html.src, function() {
     gulp.src(paths.html.src);
   });
-  gulp.watch(paths.js.src, ['js-build', 'js-sourcemaps', 'js-inject']);
-  gulp.watch(paths.sass.src, ['sass-build', 'sass-sourcemaps']);
+  gulp.watch(paths.scripts.src, ['scripts-build', 'scripts-sourcemaps', 'scripts-inject']);
+  gulp.watch(paths.styles.src, ['styles-build', 'styles-sourcemaps']);
 });
 
 /**
@@ -108,4 +139,4 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default', ['webserver', 'watch', 'js-build', 'js-sourcemaps', 'js-inject']);
+gulp.task('default', ['webserver', 'watch', 'scripts-build', 'scripts-sourcemaps', 'inject']);
