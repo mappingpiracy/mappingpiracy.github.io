1ead7352934c6eb0a4d8f8629e6d588297050914
Alex Klibisz <aklibisz@utk.edu>
2015-08-31 02:57:11 -0400

    replaced gulp-webserver with gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-  webserver = require('gulp-webserver'),
   sass = require('gulp-sass'),
   inject = require('gulp-inject'),
   uglify = require('gulp-uglify'),
@@ -8,7 +7,8 @@ var gulp = require('gulp'),
   gsourcemaps = require('gulp-sourcemaps'),
   gstripdebug = require('gulp-strip-debug'),
   gcssmin = require('gulp-cssmin'),
-  gangulartemplates = require('gulp-angular-templates');
+  gangulartemplates = require('gulp-angular-templates')
+  gconnect = require('gulp-connect');
 
 var paths = {
   base: './',
@@ -26,26 +26,6 @@ var paths = {
   styles: {
     src: ['./app/components/**/*.scss'],
     min: 'app.min.css'
-  },
-  lib: {
-    scripts: {
-      src: [
-        'app/lib/angular/angular.js',
-        'app/lib/angular-route/angular-route.js',
-        'app/lib/jquery/dist/jquery.js',
-        'app/lib/d3/d3.js',
-        'app/lib/nvd3/nv.d3.min.js',
-        'app/lib/leaflet/dist/leaflet.js',
-        'app/lib/file-saver/FileSaver.min.js',
-        'app/lib/sheetrock/dist/sheetrock.min.js',
-        'app/lib/angular-ui-bootstrap/ui-bootstrap-custom-0.13.3.min.js',
-        'app/lib/angular-ui-bootstrap/ui-bootstrap-custom-tpls-0.13.3.min.js',
-        'app/lib/moment/min/moment.min.js',
-        'app/lib/modernizr/modernizr.js',
-        'app/lib/webshim/scripts-webshim/minified/polyfiller.js'
-      ],
-      min: 'lib.min.js'
-    }
   }
 };
 
@@ -64,7 +44,6 @@ gulp.task('html-build', function() {
 gulp.task('inject', function() {
   var target = gulp.src(paths.index),
     sources = gulp.src([
-      // paths.build + '/' + paths.lib.scripts.min,
       paths.build + '/' + paths.scripts.min,
       paths.build + '/' + paths.styles.min
     ], {
@@ -72,13 +51,6 @@ gulp.task('inject', function() {
     });
   return target.pipe(inject(sources))
     .pipe(gulp.dest(paths.base));
-});
-
-gulp.task('lib-build', function() {
-  gulp.src(paths.lib.scripts.src)
-    .pipe(gconcat(paths.lib.scripts.min))
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('scripts-build', function() {
@@ -120,23 +92,19 @@ gulp.task('styles-sourcemaps', function() {
  * Watches and reloads html, css, js files when any changes are made
  */
 gulp.task('watch', function() {
-  gulp.watch(paths.html.src, function() {
-    gulp.src(paths.html.src);
+  gulp.watch(paths.scripts.src, ['scripts-build', 'scripts-sourcemaps', 'inject']);
+  gulp.watch(paths.styles.src, ['styles-build', 'styles-sourcemaps', 'inject']);
+  gulp.watch(paths.html.src, ['html-build']);
+  gulp.watch(paths.index, function() {
+    gulp.src(paths.index)
+      .pipe(gconnect.reload());
   });
-  gulp.watch(paths.scripts.src, ['scripts-build', 'scripts-sourcemaps', 'scripts-inject']);
-  gulp.watch(paths.styles.src, ['styles-build', 'styles-sourcemaps']);
 });
 
-/**
- * Launches a local webserver to run the angular app
- */
-gulp.task('webserver', function() {
-  gulp.src(paths.base)
-    .pipe(webserver({
-      livereload: true,
-      open: true,
-      port: 9898
-    }));
+gulp.task('connect', function() {
+  gconnect.server({
+    livereload: true
+  });
 });
 
-gulp.task('default', ['webserver', 'watch', 'scripts-build', 'scripts-sourcemaps', 'inject']);
+gulp.task('default', ['connect', 'watch', 'html-build', 'scripts-build', 'scripts-sourcemaps', 'styles-build', 'inject']);
